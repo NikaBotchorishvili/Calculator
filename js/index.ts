5ef6b8b8fef3875995a5f48fb7a29e2f4793f3b2
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-03-31 23:20:27 +0400

    Calculator functionallity working but with some bugs

--- js/index.ts
+++ js/index.ts
@@ -1,32 +1,40 @@
-const items = document.querySelectorAll(".item");
 const inputBox = document.getElementById("input-box");
+
+const items = document.querySelectorAll(".item");
 const equals = document.getElementById("equals");
-const clear = document.getElementById("clear")
+const clear = document.getElementById("clear");
 
 let action: boolean = false;
+let lastAction: string | null = "";
 
 items.forEach((item) => {
 	item.addEventListener("click", () => {
-		if (item.getAttribute("data-num")) {
-			if (inputBox != null) {
-				inputBox.innerText += item.getAttribute("data-num");
-			}
-		} else if (item.getAttribute("data-action")) {
-			if (inputBox != null) {
-				if (!action) {
-					inputBox.innerText += item.getAttribute("data-action");
-					action = true;
-				} else if (action) {
-					action = false;
-					inputBox.innerText = inputBox.innerText.slice(0, -1);
-				}
+		if (item.getAttribute("data-num") && inputBox != null) {
+			inputBox.innerText += item.getAttribute("data-num");
+		} else if (item.getAttribute("data-action") && inputBox != null) {
+			let actionValue = item.getAttribute("data-action");
+			if (!action) {
+
+				inputBox.innerText += actionValue;
+				action = true;
+				lastAction = actionValue;
+                console.log(inputBox.innerText)
+			} else if (action && actionValue != null ) {
+                action = false;
+            
+                inputBox.innerText = actionValue.slice(0, -1);
 			}
 		}
 	});
 });
 
 equals?.addEventListener("click", () => {
-    if(inputBox?.innerText != '' && inputBox != null){
-        inputBox.innerText = eval(inputBox.innerText)
-    }
-})
+	if (inputBox?.innerText != "" && inputBox != null) {
+		inputBox.innerText = eval(inputBox.innerText);
+	}
+});
+
+clear?.addEventListener("click", () => {
+	if (inputBox != null) inputBox.innerText = "";
+	action = false;
+});
