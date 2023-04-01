bf4f62be96ed9a0424b04c61d2ef9be87f7f9319
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-04-01 22:57:39 +0400

    clear functionality implemented and fixxed some bugs about double mulitiple action inputs

--- js/index.js
+++ js/index.js
@@ -5,22 +5,33 @@ const equals = document.getElementById("equals");
 const clear = document.getElementById("clear");
 let action = false;
 let lastAction = "";
+//	This piece of code loops through every item such as actions and numbers except equals and clear
 items.forEach((item) => {
+    //	The click event listener is being added on every given item
     item.addEventListener("click", () => {
+        //	If the number value exists in dataset and the input box isn't null
         if (item.getAttribute("data-num") && inputBox != null) {
+            // The number from dataset gets added to the input box value
             inputBox.innerText += item.getAttribute("data-num");
+            // 	Now we check if we can access action value in dataset and also that the input box isn't null
         }
         else if (item.getAttribute("data-action") && inputBox != null) {
             let actionValue = item.getAttribute("data-action");
-            if (!action) {
+            if (!action && inputBox.innerText != "") {
                 inputBox.innerText += actionValue;
                 action = true;
                 lastAction = actionValue;
-                console.log(inputBox.innerText);
             }
             else if (action && actionValue != null) {
-                action = false;
-                inputBox.innerText = actionValue.slice(0, -1);
+                if (lastAction != actionValue) {
+                    console.log(`Last Action: ${lastAction} CurrentAction: ${actionValue}`);
+                    inputBox.innerText = inputBox.innerText.slice(0, -1) + actionValue;
+                    lastAction = actionValue;
+                }
+                // else {
+                // 	inputBox.innerText = inputBox.innerText.slice(0, -1);
+                // 	action = false;	
+                // }
             }
         }
     });
